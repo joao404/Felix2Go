93c41d519ab606d636ef9926e5b78c8580bd6cf2
joao404 <mmaage92@googlemail.com>
2020-12-17 20:47:43 +0100

    updater for buster

--- get-debian-iso.sh
+++ get-debian-iso.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd iso
-wget http://cdimage.debian.org/debian-cd/8.4.0/amd64/iso-cd/debian-8.4.0-amd64-lxde-CD-1.iso
+wget http://cdimage.debian.org/debian-cd/10.7.0/amd64/iso-cd/debian-10.7.0-amd64-netinst.iso
